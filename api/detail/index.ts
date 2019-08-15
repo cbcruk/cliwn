29ae7044d6a8395637ae9e17c13d035f0bf408dd
cbcruk <cbcruk@gmail.com>
2019-08-15 10:59:04 +0900

    :zap: improvement: add mem

--- api/detail/index.ts
+++ api/detail/index.ts
@@ -3,15 +3,30 @@ import * as _ from 'lodash'
 import getDetail from '../module/detail'
 import sold from './sold'
 
+const mem = require('mem')
+
 const boards = {
   sold
 }
 
-export default async function(req: NowRequest, res: NowResponse) {
+const memDetail = mem(
+  async (board: string, id: string) => {
+    const response = await getDetail(`${board}/${id}`)
+    const route = boards[board as string]
+    const item = route ? route(response) : response.detail
+
+    return item
+  },
+  {
+    maxAge: 60000
+  }
+)
+
+async function detail(req: NowRequest, res: NowResponse) {
   const { board, id } = req.query
-  const response = await getDetail(`${board}/${id}`)
-  const route = boards[board as string]
-  const item = route ? route(response) : response.detail
+  const item = await memDetail(board, id)
 
   res.json(item)
 }
+
+export default detail
--- api/list/index.ts
+++ api/list/index.ts
@@ -2,15 +2,28 @@ import { NowRequest, NowResponse } from '@now/node'
 import getList from '../module/list'
 import sold from './sold'
 
+const mem = require('mem')
+
 const boards = {
   sold
 }
 
+const memList = mem(
+  async (board: string, query) => {
+    const response = await getList(board, query)
+    const route = boards[board]
+    const items = route ? route(response) : response.list
+
+    return items
+  },
+  {
+    maxAge: 60000
+  }
+)
+
 async function list(req: NowRequest, res: NowResponse) {
-  const { board } = req.query
-  const response = await getList(board, req.query)
-  const route = boards[board as string]
-  const items = route ? route(response) : response.list
+  const { board, ...rest } = req.query
+  const items = await memList(board, rest)
 
   res.json({
     items
