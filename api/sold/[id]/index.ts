ed85f91c81063841e59cf70076f772cc1dd2c9eb
cbcruk <cbcruk@gmail.com>
2020-09-13 09:51:32 +0900

    :bug: bug: trycatch문 추가

--- api/sold/[id]/index.ts
+++ api/sold/[id]/index.ts
@@ -2,15 +2,19 @@ import rpc from '../../../lib/rpc'
 import getData from '../../../lib/helper/getData'
 
 async function soldById(req, res) {
-  const { id, ...params } = req.query
-  const document = await rpc(`/sold/${id}`, {
-    params,
-  })
-  const data = getData(document)
+  try {
+    const { id, ...params } = req.query
+    const document = await rpc(`/sold/${id}`, {
+      params,
+    })
+    const data = getData(document)
 
-  res.json({
-    data,
-  })
+    res.json({
+      data,
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default soldById
--- api/sold/index.ts
+++ api/sold/index.ts
@@ -3,18 +3,22 @@ import getSoldList from '../../lib/helper/getSoldList'
 import getPageOffset from '../../lib/helper/getPageOffset'
 
 async function sold(req, res) {
-  const document = await rpc('/sold', {
-    params: req.query,
-  })
-  const data = getSoldList(document)
-  const pageOffset = getPageOffset(document)
+  try {
+    const document = await rpc('/sold', {
+      params: req.query,
+    })
+    const data = getSoldList(document)
+    const pageOffset = getPageOffset(document)
 
-  res.json({
-    data,
-    links: {
-      next: pageOffset + 1,
-    },
-  })
+    res.json({
+      data,
+      links: {
+        next: pageOffset + 1,
+      },
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default sold
